00adaca0a84c3fe269768960d85f094c08e4d565
Sébastien Lampazona <sebastien@oclock.io>
2021-03-15 14:08:04 +0100

    Ajout du react modele et nettoyage

---
+++ src/components/Fourchette/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const Fourchette = () => (
+  <div className="fourchette">
+    <h1>Composant : Fourchette</h1>
+  </div>
+);
+
+// == Export
+export default Fourchette;
